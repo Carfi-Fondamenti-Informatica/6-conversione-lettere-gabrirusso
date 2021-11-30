83cb342d98d05d71c8229d61c2b752cccc01ff2f
gabrirusso <91951339+gabrirusso@users.noreply.github.com>
2021-11-30 10:44:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
-int main(){
-  return 0;
+using namespace std;
+#include "lib.h"
+
+
+int main() {
+    char carattereinserito;
+    char stampa;
+    cin >> carattereinserito;
+
+    if (funzione(carattereinserito)==true){
+        if(carattereinserito>=65&&carattereinserito<=90) {
+            stampa=carattereinserito + 32;
+        } else{stampa=carattereinserito -32;
+    } }
+        else {
+        cout << "errore" <<endl ;
+    }
+        cout<< stampa<<endl;
+    return 0;
+
 }
