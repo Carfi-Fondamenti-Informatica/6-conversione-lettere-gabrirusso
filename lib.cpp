fce848e4356b8f8e978c618096542af803bb7be2
gabrirusso <91951339+gabrirusso@users.noreply.github.com>
2021-11-30 10:47:44 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
-
+#include "lib.h"
+bool funzione(char carattereinserito){
+    bool risultato=true;
+    if (carattereinserito>=65&&carattereinserito<=90 or carattereinserito>=97&&carattereinserito<=122){
+        risultato=true;
+    } else{
+        risultato=false;
+    }
+    return risultato;
+}
